d1b13f030c76e2cb1608cded0d3aad6d0cacada3
Jason Highet <jason.highet@gmail.com>
2016-09-25 23:03:40 +1300

    More loops

--- Halloumi.Shuffler.TestHarness/Form1.cs
+++ Halloumi.Shuffler.TestHarness/Form1.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Linq;
 using System.Windows.Forms;
 using Halloumi.Shuffler.AudioEngine.Channels;
 using Halloumi.Shuffler.AudioEngine.Helpers;
 using Halloumi.Shuffler.AudioEngine.Players;
+using Newtonsoft.Json;
+using System.IO;
+using System.Collections.Generic;
 
 namespace Halloumi.Shuffler.TestHarness
 {
@@ -17,126 +21,204 @@ namespace Halloumi.Shuffler.TestHarness
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
-
-
-            var targetBpm = 97M;
+            var module =  JsonConvert.DeserializeObject<Module>(File.ReadAllText("song.json"));
+            var targetBpm = module.Bpm;
+            var loopLength = BpmHelper.GetDefaultLoopLength(targetBpm);
 
             ChannelHelper.InitialiseAudioEngine(Handle);
-
             var speakers = new SpeakerOutputChannel();
-            _player = new AudioPlayer();
-
-            speakers.AddInputChannel(_player.Output);
-
-            var loopLength = BpmHelper.GetDefaultLoopLength(targetBpm) * 6;
-            _player.Load("Silence", SilenceHelper.GetSilenceAudioFile());
-            _player.AddSection("Silence", "Silence", 0, loopLength, bpm: targetBpm, loopIndefinitely: true);
-
-            _player.Load("JawBreaks05",
-                @"E:\Music\Library\Samples\DrumLoops.Breaks\Jaw Breaks\05 - DrumLoops.Breaks - Jaw Breaks 05.mp3");
-            _player.AddSection("JawBreaks05", "MainBreak", 6.35, 1.19, targetBpm: targetBpm, loopIndefinitely: true);
-
-            var vivaTirado = @"E:\Music\Library\100% Dynamite!\300% Dynamite!\14 - Various - Augustus Pablo  Viva Tirado.mp3";
-            _player.Load("VivaTirado1", vivaTirado);
-            _player.Load("VivaTirado2", vivaTirado);
-            _player.Load("VivaTirado3", vivaTirado);
-
-            _player.Load("VivaTirado4", vivaTirado);
-            _player.Load("VivaTirado5", vivaTirado);
-            _player.Load("VivaTirado6", vivaTirado);
-
-            _player.Load("Chorus1", vivaTirado);
-            _player.Load("Chorus2", vivaTirado);
-            _player.Load("Chorus3", vivaTirado);
 
 
-            _player.AddSection("VivaTirado1", "Loop1", 1.730, 5.09, targetBpm: targetBpm, loopIndefinitely: true);
-            _player.AddSection("VivaTirado2", "Loop2", 2.718, 5.107, 6.820, targetBpm: targetBpm, loopIndefinitely: true);
-            _player.AddSection("VivaTirado3", "Loop3", 6.810, 5.041, targetBpm: targetBpm, loopIndefinitely: true);
+            var channelPlayers = new List<AudioPlayer>();
+            foreach (var channel in module.Channels)
+            {
+                var channelPlayer = new AudioPlayer();
+                speakers.AddInputChannel(channelPlayer.Output);
+                channelPlayers.Add(channelPlayer);
 
-            _player.AddSection("VivaTirado4", "Loop4", 11.851, 5.021, targetBpm: targetBpm, loopIndefinitely: true);
-            _player.AddSection("VivaTirado5", "Loop5", 16.872, 5.005, targetBpm: targetBpm, loopIndefinitely: true);
-            _player.AddSection("VivaTirado6", "Loop6", 26.911, 5.006, targetBpm: targetBpm, loopIndefinitely: true);
+                foreach (var audioFile in module.AudioFiles)
+                {
+                    channelPlayer.Load(audioFile.Key, audioFile.Path);
 
-            _player.AddSection("Chorus1", "Chorus1",31.875, 4.975, targetBpm: targetBpm, loopIndefinitely: true);
-            _player.AddSection("Chorus2", "Chorus2", 36.83, 4.975, targetBpm: targetBpm, loopIndefinitely: true);
-            _player.AddSection("Chorus3", "Chorus3", 46.77, 4.9, targetBpm: targetBpm, loopIndefinitely: true);
+                    foreach (var sample in audioFile.Samples)
+                    {
+                        channelPlayer.AddSection(audioFile.Key, 
+                            sample.Key,
+                            sample.Start,
+                            sample.End,
+                            sample.Offset.HasValue ? sample.Offset.Value :0,
+                            targetBpm: module.Bpm);
+                    }
+                }
+            }
+            foreach (var pattern in module.Patterns)
+            {
+                var columnIndex = 0;
+                foreach (var column in pattern.Sequence)
+                {
+                    var player = channelPlayers.Where(x => channelPlayers.IndexOf(x) == columnIndex).FirstOrDefault();
+                    var columnControlKey = pattern.Key + columnIndex;
+                    player.Load(columnControlKey, SilenceHelper.GetSilenceAudioFile());
+                    player.AddSection(columnControlKey, columnControlKey, 0, loopLength, bpm: targetBpm);
+
+                    var rowIndex = 0;
+                    foreach (var row in column)
+                    {
+                        var audioFileKey = row.Split('.')[0];
+                        var sampleKey = row.Split('.')[1];
+
+                        var loopPosition = 0.0;
+                        if (rowIndex > 0 )
+                            loopPosition = rowIndex / (double)column.Count;
+
+                        var position = GetLoopPosition(loopPosition, targetBpm);
+
+                        player.AddPlayEvent(columnControlKey, position, audioFileKey, sampleKey);
+
+                        rowIndex++;
+                    }
+
+                    columnIndex++;
+                }
+            }
+            var mainPlayer = new AudioPlayer();
+            speakers.AddInputChannel(mainPlayer.Output);
+            mainPlayer.Load("Song", SilenceHelper.GetSilenceAudioFile());
+            var songLength = loopLength * module.Sequence.Count;
+            mainPlayer.AddSection("Song", "Song", 0, songLength, bpm: targetBpm);
+
+            var patternIndex = 0;
+            foreach (var patternKey in module.Sequence)
+            {
+                var position = GetLoopPosition(patternIndex, targetBpm);
+                var pattern = module.Patterns.Where(x => x.Key == patternKey).FirstOrDefault();
+
+                var columnIndex = 0;
+                foreach (var column in pattern.Sequence)
+                {
+                    var columnControlKey = patternKey + columnIndex;
+                    mainPlayer.AddPlayEvent(columnControlKey, position, columnControlKey, columnControlKey);
+                    columnIndex++;
+                }
+
+                patternIndex++;
+            }
 
 
 
-            double position = 0.0D;
-            //_player.AddPlayEvent("Silence", position, "Chorus3", "Chorus3");
+
+
+            //_player = new AudioPlayer();
+            //speakers.AddInputChannel(_player.Output);
+
+            //var length = BpmHelper.GetDefaultLoopLength(targetBpm) * 6;
+            //_player.Load("Silence", SilenceHelper.GetSilenceAudioFile());
+            //_player.AddSection("Silence", "Silence", 0, length, bpm: targetBpm, loopIndefinitely: true);
+
+            //_player.Load("JawBreaks05",
+            //    @"E:\Music\Library\Samples\DrumLoops.Breaks\Jaw Breaks\05 - DrumLoops.Breaks - Jaw Breaks 05.mp3");
+            //_player.AddSection("JawBreaks05", "MainBreak", 6.35, 1.19, targetBpm: targetBpm, loopIndefinitely: true);
+
+            //var vivaTirado = @"E:\Music\Library\100% Dynamite!\300% Dynamite!\14 - Various - Augustus Pablo  Viva Tirado.mp3";
+            //_player.Load("VivaTirado1", vivaTirado);
+            //_player.Load("VivaTirado2", vivaTirado);
+            //_player.Load("VivaTirado3", vivaTirado);
+
+            //_player.Load("VivaTirado4", vivaTirado);
+            //_player.Load("VivaTirado5", vivaTirado);
+            //_player.Load("VivaTirado6", vivaTirado);
+
+            //_player.Load("Chorus1", vivaTirado);
+            //_player.Load("Chorus2", vivaTirado);
+            //_player.Load("Chorus3", vivaTirado);
+
+
+            //_player.AddSection("VivaTirado1", "Loop1", 1.730, 5.09, targetBpm: targetBpm, loopIndefinitely: true);
+            //_player.AddSection("VivaTirado2", "Loop2", 2.718, 5.107, 6.820, targetBpm: targetBpm, loopIndefinitely: true);
+            //_player.AddSection("VivaTirado3", "Loop3", 6.810, 5.041, targetBpm: targetBpm, loopIndefinitely: true);
+
+            //_player.AddSection("VivaTirado4", "Loop4", 11.851, 5.021, targetBpm: targetBpm, loopIndefinitely: true);
+            //_player.AddSection("VivaTirado5", "Loop5", 16.872, 5.005, targetBpm: targetBpm, loopIndefinitely: true);
+            //_player.AddSection("VivaTirado6", "Loop6", 26.911, 5.006, targetBpm: targetBpm, loopIndefinitely: true);
+
+            //_player.AddSection("Chorus1", "Chorus1",31.875, 4.975, targetBpm: targetBpm, loopIndefinitely: true);
+            //_player.AddSection("Chorus2", "Chorus2", 36.83, 4.975, targetBpm: targetBpm, loopIndefinitely: true);
+            //_player.AddSection("Chorus3", "Chorus3", 46.77, 4.9, targetBpm: targetBpm, loopIndefinitely: true);
+
+
+
+            //double position = 0.0D;
+            ////_player.AddPlayEvent("Silence", position, "Chorus3", "Chorus3");
+            ////_player.AddPlayEvent("Silence", position, "JawBreaks05", "MainBreak");
+
+
+
+            //_player.AddEvent("Silence", position, "Chorus3", "Chorus3", EventType.Pause);
+            //_player.AddPlayEvent("Silence", position, "VivaTirado1", "Loop1");
+            //_player.AddPlayEvent("Silence", position, "JawBreaks05", "MainBreak");
+
+            //position = GetLoopPosition(0.5, targetBpm);
+            //_player.AddEvent("Silence", position, "VivaTirado1", "Loop1", EventType.Pause);
+            //_player.AddPlayEvent("Silence", position, "VivaTirado2", "Loop2");
+            //_player.AddPlayEvent("Silence", position, "JawBreaks05", "MainBreak");
+
+            //position = GetLoopPosition(1, targetBpm);
+            //_player.AddEvent("Silence", position, "VivaTirado2", "Loop2", EventType.Pause);
+            //_player.AddPlayEvent("Silence", position, "VivaTirado3", "Loop3");
+            //_player.AddPlayEvent("Silence", position, "JawBreaks05", "MainBreak");
+
+            //position = GetLoopPosition(1.5, targetBpm);
+            //_player.AddEvent("Silence", position, "VivaTirado3", "Loop3", EventType.Pause);
+            //_player.AddPlayEvent("Silence", position, "VivaTirado2", "Loop2");
+            //_player.AddPlayEvent("Silence", position, "JawBreaks05", "MainBreak");
+
+
+            //position = GetLoopPosition(2, targetBpm);
+            //_player.AddEvent("Silence", position, "VivaTirado2", "Loop2", EventType.Pause);
+            //_player.AddPlayEvent("Silence", position, "VivaTirado4", "Loop4");
+            //_player.AddPlayEvent("Silence", position, "JawBreaks05", "MainBreak");
+
+            //position = GetLoopPosition(2.5, targetBpm);
+            //_player.AddEvent("Silence", position, "VivaTirado4", "Loop4", EventType.Pause);
+            //_player.AddPlayEvent("Silence", position, "VivaTirado5", "Loop5");
+            //_player.AddPlayEvent("Silence", position, "JawBreaks05", "MainBreak");
+
+            //position = GetLoopPosition(3, targetBpm);
+            //_player.AddEvent("Silence", position, "VivaTirado5", "Loop5", EventType.Pause);
+            //_player.AddPlayEvent("Silence", position, "VivaTirado4", "Loop4");
+            //_player.AddPlayEvent("Silence", position, "JawBreaks05", "MainBreak");
+
+            //position = GetLoopPosition(3.5, targetBpm);
+            //_player.AddEvent("Silence", position, "VivaTirado4", "Loop4", EventType.Pause);
+            //_player.AddPlayEvent("Silence", position, "VivaTirado6", "Loop6");
             //_player.AddPlayEvent("Silence", position, "JawBreaks05", "MainBreak");
 
 
 
-            _player.AddEvent("Silence", position, "Chorus3", "Chorus3", EventType.Pause);
-            _player.AddPlayEvent("Silence", position, "VivaTirado1", "Loop1");
-            _player.AddPlayEvent("Silence", position, "JawBreaks05", "MainBreak");
 
-            position = GetLoopPosition(0.5, targetBpm);
-            _player.AddEvent("Silence", position, "VivaTirado1", "Loop1", EventType.Pause);
-            _player.AddPlayEvent("Silence", position, "VivaTirado2", "Loop2");
-            _player.AddPlayEvent("Silence", position, "JawBreaks05", "MainBreak");
+            //position = GetLoopPosition(4, targetBpm);
+            //_player.AddEvent("Silence", position, "VivaTirado6", "Loop6", EventType.Pause);
+            //_player.AddPlayEvent("Silence", position, "Chorus1", "Chorus1");
+            //_player.AddPlayEvent("Silence", position, "JawBreaks05", "MainBreak");
 
-            position = GetLoopPosition(1, targetBpm);
-            _player.AddEvent("Silence", position, "VivaTirado2", "Loop2", EventType.Pause);
-            _player.AddPlayEvent("Silence", position, "VivaTirado3", "Loop3");
-            _player.AddPlayEvent("Silence", position, "JawBreaks05", "MainBreak");
+            //position = GetLoopPosition(4.5, targetBpm);
+            //_player.AddEvent("Silence", position, "Chorus1", "Chorus1", EventType.Pause);
+            //_player.AddPlayEvent("Silence", position, "Chorus2", "Chorus2");
+            //_player.AddPlayEvent("Silence", position, "JawBreaks05", "MainBreak");
 
-            position = GetLoopPosition(1.5, targetBpm);
-            _player.AddEvent("Silence", position, "VivaTirado3", "Loop3", EventType.Pause);
-            _player.AddPlayEvent("Silence", position, "VivaTirado2", "Loop2");
-            _player.AddPlayEvent("Silence", position, "JawBreaks05", "MainBreak");
+            //position = GetLoopPosition(5, targetBpm);
+            //_player.AddEvent("Silence", position, "Chorus2", "Chorus2", EventType.Pause);
+            //_player.AddPlayEvent("Silence", position, "Chorus1", "Chorus1");
+            //_player.AddPlayEvent("Silence", position, "JawBreaks05", "MainBreak");
 
-
-            position = GetLoopPosition(2, targetBpm);
-            _player.AddEvent("Silence", position, "VivaTirado2", "Loop2", EventType.Pause);
-            _player.AddPlayEvent("Silence", position, "VivaTirado4", "Loop4");
-            _player.AddPlayEvent("Silence", position, "JawBreaks05", "MainBreak");
-
-            position = GetLoopPosition(2.5, targetBpm);
-            _player.AddEvent("Silence", position, "VivaTirado4", "Loop4", EventType.Pause);
-            _player.AddPlayEvent("Silence", position, "VivaTirado5", "Loop5");
-            _player.AddPlayEvent("Silence", position, "JawBreaks05", "MainBreak");
-
-            position = GetLoopPosition(3, targetBpm);
-            _player.AddEvent("Silence", position, "VivaTirado5", "Loop5", EventType.Pause);
-            _player.AddPlayEvent("Silence", position, "VivaTirado4", "Loop4");
-            _player.AddPlayEvent("Silence", position, "JawBreaks05", "MainBreak");
-
-            position = GetLoopPosition(3.5, targetBpm);
-            _player.AddEvent("Silence", position, "VivaTirado4", "Loop4", EventType.Pause);
-            _player.AddPlayEvent("Silence", position, "VivaTirado6", "Loop6");
-            _player.AddPlayEvent("Silence", position, "JawBreaks05", "MainBreak");
+            //position = GetLoopPosition(5.5, targetBpm);
+            //_player.AddEvent("Silence", position, "Chorus1", "Chorus1", EventType.Pause);
+            //_player.AddPlayEvent("Silence", position, "Chorus3", "Chorus3");
+            //_player.AddPlayEvent("Silence", position, "JawBreaks05", "MainBreak");
 
 
-
-
-            position = GetLoopPosition(4, targetBpm);
-            _player.AddEvent("Silence", position, "VivaTirado6", "Loop6", EventType.Pause);
-            _player.AddPlayEvent("Silence", position, "Chorus1", "Chorus1");
-            _player.AddPlayEvent("Silence", position, "JawBreaks05", "MainBreak");
-
-            position = GetLoopPosition(4.5, targetBpm);
-            _player.AddEvent("Silence", position, "Chorus1", "Chorus1", EventType.Pause);
-            _player.AddPlayEvent("Silence", position, "Chorus2", "Chorus2");
-            _player.AddPlayEvent("Silence", position, "JawBreaks05", "MainBreak");
-
-            position = GetLoopPosition(5, targetBpm);
-            _player.AddEvent("Silence", position, "Chorus2", "Chorus2", EventType.Pause);
-            _player.AddPlayEvent("Silence", position, "Chorus1", "Chorus1");
-            _player.AddPlayEvent("Silence", position, "JawBreaks05", "MainBreak");
-
-            position = GetLoopPosition(5.5, targetBpm);
-            _player.AddEvent("Silence", position, "Chorus1", "Chorus1", EventType.Pause);
-            _player.AddPlayEvent("Silence", position, "Chorus3", "Chorus3");
-            _player.AddPlayEvent("Silence", position, "JawBreaks05", "MainBreak");
-
-
-            _player.QueueSection("Silence", "Silence");
-            _player.Play("Silence");
+            //_player.QueueSection("Silence", "Silence");
+            //_player.Play("Silence");
         }
 
         private double GetLoopPosition(double loopCount, decimal bpm)
---
+++ Halloumi.Shuffler.TestHarness/Module.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace Halloumi.Shuffler.TestHarness
+{
+    public class Module
+    {
+        public string Title { get; set; }
+        public decimal Bpm { get; set; }
+        public List<AudioFile> AudioFiles { get; set; }
+        public List<Channel> Channels { get; set; }
+        public List<Pattern> Patterns { get; set; }
+        public List<string> Sequence { get; set; }
+
+        public class Sample
+        {
+            public string Key { get; set; }
+            public double Start { get; set; }
+            public double End { get; set; }
+            public double? Offset { get; set; }
+        }
+
+        public class AudioFile
+        {
+            public string Key { get; set; }
+            public string Path { get; set; }
+            public List<Sample> Samples { get; set; }
+        }
+
+        public class Channel
+        {
+            public string Key { get; set; }
+        }
+
+        public class Pattern
+        {
+            public string Key { get; set; }
+            public List<List<string>> Sequence { get; set; }
+        }
+    }
+}
