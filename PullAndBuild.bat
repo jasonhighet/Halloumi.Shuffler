0286f027d8aa89c5c24af82303f44f214ed2d62d
Jason Highet <jason.highet@gmail.com>
2016-10-29 10:37:40 +1300

    Pull and build

---
+++ PullAndBuild.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Git\bin\git.exe" pull origin master
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\MsBuild.exe" Halloumi.Shuffler.sln /t:Build /p:Configuration=Release /p:Platform="x86" /m
