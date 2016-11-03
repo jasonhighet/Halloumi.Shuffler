6f99361437797ac93ab104f5318e8fc9be079b23
Jason Highet <jason.highet@gmail.com>
2016-11-04 10:07:19 +1300

    Build

--- PullAndBuild.bat
+++ PullAndBuild.bat
@@ -1,2 +1,9 @@
-"C:\Program Files (x86)\Git\bin\git.exe" pull origin master
-"C:\Windows\Microsoft.NET\Framework\v4.0.30319\MsBuild.exe" Halloumi.Shuffler.sln /t:Build /p:Configuration=Release /p:Platform="x86" /m
+@echo off
+
+if exist "%PROGRAMFILES%\Git\bin\git.exe" (
+	"%PROGRAMFILES%\Git\bin\git.exe" pull origin master
+) else (
+	"%PROGRAMFILES(86)%\Git\bin\git.exe" pull origin master
+)
+"%PROGRAMFILES(X86)%\Microsoft Visual Studio 14.0\Common7\IDE\devenv" "Halloumi.Shuffler.sln" /Build "Release"
+pause
